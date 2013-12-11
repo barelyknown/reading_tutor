701f1b4f31e0bbc30915ae31f8cef0b8e1980bbe
barelyknown <sean@buytruckload.com>
2013-12-11 16:25:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReadingTutor::Application.config.secret_key_base = 'a7f9137911c1f16f395f298c80cbfd959ed8417740eaf1d9a334576098004ff68b4a38068e64124a7e3b4de0b0d2aa784e16a8c0d7d4b048baf41558e8549150'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReadingTutor::Application.config.session_store :cookie_store, key: '_reading_tutor_session'
